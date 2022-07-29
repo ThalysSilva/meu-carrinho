396296ecf0708175b728ea2819eda2cf6ee5482f
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-07-29 19:37:49 -0300

    fix: adjust in size of ItemCart component

--- src/components/pageCart/Item/index.tsx
+++ src/components/pageCart/Item/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ImageWithFrame } from '../../shared/ImageWithFrame';
-import { H2, H3, H4 } from '../../shared/Texts';
+import { H2, H3, H4, H5 } from '../../shared/Texts';
 
 type Props = {
   itemName: string;
@@ -11,12 +11,12 @@ type Props = {
 
 export function ItemCart({ imgSrc, itemName, price, sellPrice }: Props) {
   return (
-    <div className="flex flex-row gap-8 items-center">
+    <div className="flex flex-row gap-4 items-center">
       <ImageWithFrame src={imgSrc} width={'100px'} height={'100px'} />
       <div className={'flex flex-col gap-1'}>
         <H3>{itemName}</H3>
         <div className={'flex flex-col'}>
-          <H4 className={'font-semibold text-gray-400'}>{`R$ ${price}`}</H4>
+          <H5 className={'font-semibold text-gray-400'}>{`R$ ${price}`}</H5>
           <H4 className={'font-semibold '}>{`R$ ${sellPrice}`}</H4>
         </div>
       </div>
