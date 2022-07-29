f00865b5d8927441ebb5a901af7bc40d8ac0ff4a
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-07-29 20:29:18 -0300

    feat: create cart service

---
+++ src/services/cart/index.tsx
@@ -0,0 +1,17 @@
+import config from '../../config';
+import middleware from '../middleware';
+
+export type CartType = keyof typeof cart
+
+const { cart } = config.apiRoutes;
+
+export function cartService() {
+  async function CartItems(type: CartType) {
+    return await middleware
+      .requestAxios()
+      .get(cart[type])
+      .then((res) => res);
+  }
+
+  return { CartItems };
+}
