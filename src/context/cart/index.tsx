46e2e47feb7b02eeeba59f8362c1edb1dc3059dd
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-07-29 20:52:42 -0300

    chore: remove unused useEffect

--- src/context/cart/index.tsx
+++ src/context/cart/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useEffect, useState } from 'react';
+import React, { createContext, ReactNode, useState } from 'react';
 import { CartContextData, CartItem } from './types';
 
 type CartProviderProps = {
