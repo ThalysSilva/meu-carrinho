fa6181b7cda59b88fb07d925a1fc97b9cd868d7e
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-07-29 21:20:45 -0300

    chore: change name of cart object keys

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -1,7 +1,7 @@
 const ApiRoutes = {
   cart: {
-    lowerOrEqualThanTen: '/62e302c8248d43754f09e8dc',
-    upperThanTen: '/62e302ea248d43754f09e923'
+    lessOrEqualThanTen: '/62e302c8248d43754f09e8dc',
+    moreThanTen: '/62e302ea248d43754f09e923'
   }
 };
 
