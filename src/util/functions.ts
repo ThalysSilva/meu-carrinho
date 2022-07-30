73f080bdc83ed1b062f8afbad8eb295ba49270eb
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-07-30 01:27:40 -0300

    feat: adding some utils functions

---
+++ src/util/functions.ts
@@ -0,0 +1,10 @@
+export function normalizePrice(price: string) {
+  const priceString = price.toString()
+  const normalizedPrice = priceString.slice(0, -2) + ',' + priceString.slice(-2);
+  if (priceString.length < 3) return '0' + normalizedPrice;
+  return normalizedPrice;
+}
+
+export function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
