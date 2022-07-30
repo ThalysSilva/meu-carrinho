b922f739bea8dcc50f5c3f300c7821162c182d28
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-07-30 01:35:26 -0300

    feat: adding onclick and use Hook of pageHome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,39 @@ import type { NextPage } from 'next';
 import { Container } from '../components/shared/Container';
 import { Button } from '../components/shared/Button';
 import { H1 } from '../components/shared/Texts';
+import useHome from '../hooks/pageHome/useHome';
 
 const Home: NextPage = () => {
+  const { onClick, isLoading } = useHome();
   return (
     <Container
       className={
         'flex flex-col relative w-full h-screen justify-center sm:h-fit sm:w-[640px] sm:rounded-2xl sm:shadow-2xl min-w-[360px] bg-white  py-4'
       }
     >
-      <div className={'flex w-full items-center justify-center mb-4'}>
+      <div className={'flex w-full items-center justify-center mb-10 sm:mb-4'}>
         <H1>{'Escolha o exemplo'}</H1>
       </div>
 
-      <div className={'flex flex-col sm:flex-row gap-4 h-[100px]'}>
-        <Button dataTestId="lessThanTenButton" type="button" isLoading={false}>
-          <div className="flex justify-center items-center w-full h-60 sm:h-auto">
+      <div className={'flex flex-col sm:flex-row gap-10 sm:gap-4 px-4'}>
+        <Button
+          onClick={() => onClick('lessOrEqualThanTen')}
+          dataTestId="lessThanTenButton"
+          type="button"
+          isLoading={isLoading}
+        >
+          <div className="flex justify-center items-center w-full h-[100px]">
             {'Exemplo de carrinho MENOR que R$10,00'}
           </div>
         </Button>
 
-        <Button dataTestId="moreThanTenButton" type="button" isLoading={false}>
-          <div className="flex justify-center items-center w-full h-60 sm:h-auto">
+        <Button
+          onClick={() => onClick('moreThanTen')}
+          dataTestId="moreThanTenButton"
+          type="button"
+          isLoading={isLoading}
+        >
+          <div className="flex justify-center items-center w-full h-[100px]">
             {'Exemplo de carrinho MAIOR que R$10,00'}
           </div>
         </Button>
